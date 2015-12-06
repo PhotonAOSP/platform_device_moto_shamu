582f23d6974f43e9cca4eaec2c1d681bd228e47a
dhacker29 <dhackerdvm@gmail.com>
2015-12-06 10:08:41 -0500

    shamu: advertise that we use native exfat and ntfs drivers for otg

--- BoardConfigPhoton.mk
+++ BoardConfigPhoton.mk
@@ -4,3 +4,6 @@ TARGET_KERNEL_SOURCE := kernel/moto/shamu
 BOARD_KERNEL_IMAGE_NAME := zImage-dtb
 KERNEL_TOOLCHAIN_PREFIX := arm-eabi-
 KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_OS)-x86/arm/arm-eabi-4.8/bin
+#OTG
+TARGET_KERNEL_HAVE_EXFAT := true
+TARGET_KERNEL_HAVE_NTFS := true
