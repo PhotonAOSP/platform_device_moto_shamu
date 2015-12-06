8fa474f4c855fd3d18b665e4c877149518efae9b
Shawn W <wahi.shawn@gmail.com>
2015-12-05 23:28:39 -0500

    Add toolchain parameters for inline kernel building

--- BoardConfigPhoton.mk
+++ BoardConfigPhoton.mk
@@ -1,4 +1,6 @@
 # Inline kernel
-BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
-TARGET_KERNEL_SOURCE := kernel/moto/shamu
 TARGET_KERNEL_CONFIG := shamu_defconfig
+TARGET_KERNEL_SOURCE := kernel/moto/shamu
+BOARD_KERNEL_IMAGE_NAME := zImage-dtb
+KERNEL_TOOLCHAIN_PREFIX := arm-eabi-
+KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_OS)-x86/arm/arm-eabi-4.8/bin
